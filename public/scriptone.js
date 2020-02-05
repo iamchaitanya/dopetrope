3cd1e87e33c0889ae152bd99237bc6c7fd649e37
chaitanya <chaitanya26aug@gmail.com>
2020-02-05 23:19:12 +0530

    second get

---
+++ public/scriptone.js
@@ -0,0 +1,16 @@
+function display() {
+  var x = document.getElementById("tuesday");
+  var y = document.getElementById("joinNowButtonTuesday");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  //   if (y.style.backgroundColor === "#252122") {
+  //     y.style.backgroundColor = "#AA0000";
+  //   } else {
+  //     y.style.backgroundColor = "#252122";
+  //   }
+}
